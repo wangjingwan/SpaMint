8c65660ed3e2c7cff51a2cf1fa837de8e4ee0b56
Jingwan WANG <49140826+wangjingwan@users.noreply.github.com>
2024-07-09 10:51:27 +0800

    Update utils.py

--- spamint/utils.py
+++ spamint/utils.py
@@ -215,6 +215,3 @@ def check_decon_type(weight, sc_adata, cell_type_key):
             f'Cell type in weight matrix is different from single-cell meta file.')
     if 'celltype' not in sc_adata.obs.columns:
       sc_adata.obs['celltype'] = sc_adata.obs[cell_type_key]
-
-
-
